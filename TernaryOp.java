5d720306e984403ad82ccea9c496e20f1d42f669
SUBHAM PRASAD PANDA <79370554+Subham1901@users.noreply.github.com>
2021-02-24 19:17:40 +0530

    Add files via upload

---
+++ TernaryOp.java
@@ -0,0 +1,11 @@
+public class TernaryOp{
+	public static void main(String[] args){
+		int a=10;
+		int b=12;
+		
+		int Result= (b > a)? 12:10;//ternary operator at first it will check condition
+									//then if condition gets true it will print 1st value(true)
+									//if condition gets false it will print 2nd value(false).
+		System.out.println(Result);
+	}
+}
