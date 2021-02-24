8f6b0aa7ddf557fdcab4352f6c26bd63b7a62c72
SUBHAM PRASAD PANDA <79370554+Subham1901@users.noreply.github.com>
2021-02-24 19:18:28 +0530

    Update TernaryOp.java

--- TernaryOp.java
+++ TernaryOp.java
@@ -3,9 +3,9 @@ public class TernaryOp{
 		int a=10;
 		int b=12;
 		
-		int Result= (b > a)? 12:10;//ternary operator at first it will check condition
-									//then if condition gets true it will print 1st value(true)
-									//if condition gets false it will print 2nd value(false).
+		int Result= (b > a)? 12:10;		//ternary operator at first it will check condition
+							//then if condition gets true it will print 1st value(true)
+							//if condition gets false it will print 2nd value(false).
 		System.out.println(Result);
 	}
 }
